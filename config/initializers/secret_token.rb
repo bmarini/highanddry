42fa87dab841ef2d66bc75ee955a46063f118a78
Ben Marini <ben.marini@akqa.com>
2011-11-05 11:53:58 -0700

    Initial rails 3.1.1 install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Highanddry::Application.config.secret_token = '1ccbf370265e15c98074b53bdfeec5f0fa14d59fa865818c122ec467b4fe6052c71d2db8d7ccd9b841f19c66a24de3322de3a778d4e0d2e8f6a4cbb6b072ebf2'
